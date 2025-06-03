90b7367b948e32cbcb0cccc35e43aaedba044f6d
David Stakic <davidstakic2003@gmail.com>
2025-06-03 18:40:23 +0200

    Project Initialization

---
+++ OnlineListings/src/main/java/com/example/OnlineListings/OnlineListingsApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnlineListings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineListingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineListingsApplication.class, args);
+	}
+
+}
---
+++ OnlineListings/src/test/java/com/example/OnlineListings/OnlineListingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnlineListings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineListingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
